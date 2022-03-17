90942275634f647b391e2b2cf596a2679456ed0a
ziyacaylan <ziyacaylan@gmail.com>
2022-03-18 00:19:24 +0300

    DataAccessLayer Context sınıfının oluşturulması

---
+++ DataAccessLayer/Concreate/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concreate;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Concreate
+{
+    public class Context : DbContext      // ilk başta access modifier kısmını public yapıyoruz ki diğer taraflardan ulaşabilelim. DbContext sınıfından kalıtım alıyor
+    {
+        public DbSet<About> Abouts { get; set; } // "Abouts" sınıfın veri tabanına yansıyacak ismini göstermektedir.
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<ImageFile> ImageFiles { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
